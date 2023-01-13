d7a2c5a1942485c01bb82c5479ed0452d631d79e
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2023-01-13 13:13:03 -0700

    fix opening links in default browser

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ PLATFORM="linux"
 ARCH="x64"
 NAME="Gmail"
 
-nativefier https://mail.google.com/ --name ${NAME} --honest --internal-urls "(.*?mail\.google\.com.*?|.*?accounts\.google\.com.*?)"
+nativefier https://mail.google.com/ --name ${NAME} --honest --strict-internal-urls
 
 # Change package name from 'gmail-nativefier-XXXXXX' to 'Gmail'
 sed -i -E "s/gmail-nativefier-.{6}/${NAME}/" ${NAME}-${PLATFORM}-${ARCH}/resources/app/package.json
