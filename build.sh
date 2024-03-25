50e02a67dd7a370d0e1ffd3660f8efdeadbabd0c
Jeff Fredrickson <jeff@ejeff.org>
2024-03-24 18:28:09 -0600

    fix permissions after build

--- build.sh
+++ build.sh
@@ -3,9 +3,14 @@
 PLATFORM="linux"
 ARCH="x64"
 NAME="Gmail"
+DIR="${NAME}-${PLATFORM}-${ARCH}"
 
 nativefier https://mail.google.com/ --name ${NAME} --honest --strict-internal-urls
 
 # Change package name from 'gmail-nativefier-XXXXXX' to 'Gmail'
-sed -i -E "s/gmail-nativefier-.{6}/${NAME}/" ${NAME}-${PLATFORM}-${ARCH}/resources/app/package.json
+sed -i -E "s/gmail-nativefier-.{6}/${NAME}/" ${DIR}/resources/app/package.json
+
+# Fix permissions
+find ${DIR} -type f -exec chmod go+r {} \;
+find ${DIR} -type d -exec chmod go+rx {} \;
 
